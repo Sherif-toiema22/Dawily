c08c4d4ec4b4102454f6384909f1362e81bd9563
Sherif-toiema22 <sherif.toiema22@gmail.com>
2026-02-16 22:44:58 +0200

    re-organiser the code validation

--- Dawily/patient/src/main/java/com/microservices/patient/model/dto/PatientDTO.java
+++ Dawily/patient/src/main/java/com/microservices/patient/model/dto/PatientDTO.java
@@ -2,7 +2,6 @@ package com.microservices.patient.model.dto;
 
 import lombok.*;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PatientDTO {
@@ -10,4 +9,36 @@ public class PatientDTO {
     private String patientName;
     private Integer patientAge;
 
+    @Override
+    public String toString() {
+        return "PatientDTO{" +
+                "patientId=" + patientId +
+                ", patientName='" + patientName + '\'' +
+                ", patientAge=" + patientAge +
+                '}';
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public Integer getPatientAge() {
+        return patientAge;
+    }
+
+    public void setPatientAge(Integer patientAge) {
+        this.patientAge = patientAge;
+    }
 }
