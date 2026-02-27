8733e0f856789592277094b3d1396bc57ee8ccc0
Sherif-toiema22 <sherif.toiema22@gmail.com>
2026-02-27 23:44:49 +0200

    Adding open feign

--- Dawily/doctor/src/main/java/com/microservices/doctor/service/PatientService.java
+++ Dawily/doctor/src/main/java/com/microservices/doctor/service/PatientService.java
@@ -1,14 +1,13 @@
 package com.microservices.Doctor.service;
 
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PatientService {
-    public String getPatient(String name){
-        String fullUrl="http://localhost:9000/search/"+name;
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(
-                fullUrl, String.class);
+
+
+    public String getPatient(String ahmed) {
+        return ahmed;
     }
 }
